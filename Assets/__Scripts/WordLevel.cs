5bdbb7839f0f0e50c6b09e016a4cbf5892024b36
bursalma <32002049+bursalma@users.noreply.github.com>
2017-12-09 21:59:08 -0500

    Page 669

---
+++ Assets/__Scripts/WordLevel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class WordLevel {
+
+    public int levelNum;
+    public int longWordIndex;
+    public string word;
+    public Dictionary<char, int> charDict;
+    public List<string> subWords;
+
+    static public Dictionary<char,int> MakeCharDict(string w)
+    {
+        Dictionary<char, int> dict = new Dictionary<char, int>();
+        char c;
+        for(int i = 0; i < w.Length; i++)
+        {
+            c = w[i];
+            if (dict.ContainsKey(c))
+            {
+                dict[c]++;
+            }
+            else
+            {
+                dict.Add(c, 1);
+            }
+        }
+        return (dict);
+    }
+
+    public static bool CheckWordInLevel(string str,WordLevel level)
+    {
+        Dictionary<char, int> counts = new Dictionary<char, int>();
+    }
+}
